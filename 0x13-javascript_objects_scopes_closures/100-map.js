6544a34bca3537ed36267aa833b3578f6a10ec04
Biruk <biruk.tadesse.tefera@gmail.com>
2022-10-04 19:27:45 +0300

    Write a script that imports an array and computes a new array.  Your script must import list from the file 100-data.js You must use a map. Tips A new list must be created with each value equal to the value of the initial list, multipled by the index in the list Print both the initial list and the new list

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-let list = require('./100-data.js').list;
+
+const list = require('./100-data').list;
+
+const arr = list.map((x, i) => x * i);
+
 console.log(list);
-let newList = list.map((x, index) => x * index);
-console.log(newList);
+console.log(arr);
